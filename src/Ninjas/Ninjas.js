14f068144f7b9ebf09b5ac5818b5612e7311257c
AyushAgrawal25 <agrawal.ayush2500@gmail.com>
2020-03-07 10:24:53 +0530

    Just Started

--- src/Ninjas/Ninjas.js
+++ src/Ninjas/Ninjas.js
@@ -1,7 +1,11 @@
 import React, {Component} from "react";
 
 class Ninjas extends Component {
-    
+    render(){
+        <div className="ninjas">
+            
+        </div>
+    }
 };
 
 export default Ninjas;
